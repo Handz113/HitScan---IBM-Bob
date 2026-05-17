e528a446c8197c3536bd8d878658b685519b8821
Juan <juanm20091@gmail.com>
2026-05-16 22:12:45 -0600

    Delete app directory

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Code Analysis Demo Tool
-"""
-__version__ = "1.0.0"
-
-# Made with Bob
--- app/risk/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Risk assessment modules
-"""
-from .calculator import RiskCalculator
-from .matrix import RiskMatrixGenerator
-
-__all__ = ['RiskCalculator', 'RiskMatrixGenerator']
-
-# Made with Bob
